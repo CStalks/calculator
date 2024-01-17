597463e601879a9ab7dbb6d2c059530e8da96195
CStalks <swiafe.dev@gmail.com>
2024-01-16 20:44:32 -0500

    Add equal sign functionality

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-let input = '';
+let input = '0';
 let operand = '';
 let exprArr = [];
 let result = 0;
 let negativeSymbol  = '';
+let preVal = '';
+let prevOp = '';
 
 const buttons = document.querySelectorAll('.number');
 const display = document.querySelector('.display');
@@ -40,10 +42,11 @@ function operate(num1, operator, num2){
 operators.forEach(operator => {
     operator.addEventListener('click', () => {
         operand = operator.textContent;
+        prevOp = operand;
 
         if(input){
-            exprArr.push(input);
-            console.log(exprArr);
+            exprArr.push(+input);
+            preVal = input;
             input = '';
         }
 
@@ -57,19 +60,20 @@ operators.forEach(operator => {
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        clear.textContent = 'C';
-
-        if(operand){  
+        if(operand){ 
             exprArr.push(operand);
             operand = '';
         }
 
-        input += button.textContent;
-        display.textContent = input;
+        //add operand to exprArr only if theres already a number entered
+        if(input.length < 9){
+            if(input === '0') input = '';
+
+            input += button.textContent;
+            display.textContent = input;
+        }
     });
 });
-
-
   
 //clears display and reset everything when clicked
 clear.addEventListener('click', () => {
@@ -78,11 +82,15 @@ clear.addEventListener('click', () => {
     input = operand = '';
     exprArr = [];
     quotient = 0;
+    preVal = '';
+    prevOp = '';
 });
-
 
 negativeSign.addEventListener('click', () => {
     //fix the default display 0 bug
+    if(input == 0){
+
+    }
     if(input < 0){
         input = input.slice(1);
         display.textContent = input;
@@ -92,5 +100,59 @@ negativeSign.addEventListener('click', () => {
         display.textContent = input;
     }
 });
+
+percent.addEventListener('click', () => {
+    input = input / 100;
+    display.textContent = input;
+});
  
+equal.addEventListener('click', () => {
+
+    if(typeof exprArr[0] === 'string'){
+        if(preVal){
+            console.log(exprArr);
+            exprArr[1] = exprArr[0];
+            exprArr[0] = +preVal;
+            console.log(exprArr)
+        }
+    }
+    
+    //add operand to exprArr only if there is some number in the exprArr
+    if(operand){  
+        exprArr.push(operand);
+        operand = '';
+    }
+
+    //when there is a number inside the input store it in array and make it empty
+    if(input){
+        exprArr.push(+input);
+        preVal = input;
+        input = '';
+    }
+
+    //executes using prev value and operator when the equal button is being 
+    //clicked any number of times 
+    if(exprArr.length == 1){
+        if(prevOp && preVal){
+            exprArr.push(prevOp);
+            exprArr.push(+preVal);  
+
+            result = operate(+exprArr[0], exprArr[1], +exprArr[2]);
+            display.textContent = result;
+            exprArr = [result];
+        }
+    }
    
+    if(exprArr.length == 2){
+        result = operate(+exprArr[0], exprArr[1], +preVal);
+        display.textContent = result;
+        exprArr = [result];
+    }
+
+   //execute when user input two numbers and one operator
+    if(exprArr.length == 3){
+        result = operate(+exprArr[0], exprArr[1], +preVal);
+        display.textContent = result;
+        exprArr = [result];
+    }
+});
